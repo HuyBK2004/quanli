a53010bf038928983b9f9120a58c7e0c18b1a79d
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 03:33:08 +0700

    Update GioHang.cs

--- ChuongTrinhQuanLy/DTO/GioHang.cs
+++ ChuongTrinhQuanLy/DTO/GioHang.cs
@@ -2,34 +2,7 @@ using System.Collections.Generic;
 
 public class GioHang
 {
-    public List<ChiTietHoaDon> DanhSachHang { get; set; } = new List<ChiTietHoaDon>();
-
-    public void ThemSanPham(SanPham sp, int soLuong)
-    {
-        var spDaCo = DanhSachHang.Find(h => h.MaSanPham == sp.MaSanPham);
-        if (spDaCo != null)
-            spDaCo.SoLuong += soLuong;
-        else
-            DanhSachHang.Add(new ChiTietHoaDon {
-                MaSanPham = sp.MaSanPham,
-                TenSanPham = sp.TenSanPham,
-                DonGia = sp.Gia,
-                SoLuong = soLuong
-            });
-    }
-    public void XoaSanPham(string maSanPham)
-    {
-        DanhSachHang.RemoveAll(h => h.MaSanPham == maSanPham);
-    }
-    public void Clear()
-    {
-        DanhSachHang.Clear();
-    }
-    public decimal TongTien()
-    {
-        decimal tong = 0;
-        foreach (var cthd in DanhSachHang)
-            tong += cthd.DonGia * cthd.SoLuong;
-        return tong;
-    }
+    public int MaNguoiDung { get; set; }
+    public int MaSP { get; set; }
+    public int SoLuong { get; set; }
 }
