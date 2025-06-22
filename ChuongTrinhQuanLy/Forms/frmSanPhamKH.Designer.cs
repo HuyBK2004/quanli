e534021cd262f852ff4f7a8a9c1ef0023beee7d6
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 04:06:55 +0700

    Update frmSanPhamKH.Designer.cs

--- ChuongTrinhQuanLy/Forms/frmSanPhamKH.Designer.cs
+++ ChuongTrinhQuanLy/Forms/frmSanPhamKH.Designer.cs
@@ -29,56 +29,43 @@
         private void InitializeComponent()
         {
             this.dgvSanPham = new System.Windows.Forms.DataGridView();
-            this.lblTitle = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // dgvSanPham
-            // 
-            this.dgvSanPham.AllowUserToAddRows = false;
-            this.dgvSanPham.AllowUserToDeleteRows = false;
-            this.dgvSanPham.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvSanPham.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSanPham.Location = new System.Drawing.Point(12, 50);
-            this.dgvSanPham.Name = "dgvSanPham";
-            this.dgvSanPham.ReadOnly = true;
-            this.dgvSanPham.RowHeadersVisible = false;
-            this.dgvSanPham.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvSanPham.Size = new System.Drawing.Size(660, 340);
-            this.dgvSanPham.TabIndex = 0;
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.Location = new System.Drawing.Point(230, 9);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(229, 30);
-            this.lblTitle.TabIndex = 1;
-            this.lblTitle.Text = "Danh Sách Sản Phẩm";
-            // 
-            // frmSanPhamKH
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 411);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.dgvSanPham);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmSanPhamKH";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Sản phẩm";
-            this.Load += new System.EventHandler(this.frmSanPhamKH_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+    this.btnXemGioHang = new System.Windows.Forms.Button();
+    ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).BeginInit();
+    this.SuspendLayout();
+    // 
+    // dgvSanPham
+    // 
+    this.dgvSanPham.AllowUserToAddRows = false;
+    this.dgvSanPham.AllowUserToDeleteRows = false;
+    this.dgvSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+    this.dgvSanPham.Location = new System.Drawing.Point(12, 12);
+    this.dgvSanPham.Name = "dgvSanPham";
+    this.dgvSanPham.ReadOnly = true;
+    this.dgvSanPham.Size = new System.Drawing.Size(600, 300);
+    this.dgvSanPham.TabIndex = 0;
+    this.dgvSanPham.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSanPham_CellClick);
+    // 
+    // btnXemGioHang
+    // 
+    this.btnXemGioHang.Location = new System.Drawing.Point(520, 320);
+    this.btnXemGioHang.Name = "btnXemGioHang";
+    this.btnXemGioHang.Size = new System.Drawing.Size(92, 32);
+    this.btnXemGioHang.TabIndex = 1;
+    this.btnXemGioHang.Text = "Xem giỏ hàng";
+    this.btnXemGioHang.UseVisualStyleBackColor = true;
+    this.btnXemGioHang.Click += new System.EventHandler(this.btnXemGioHang_Click);
+    // 
+    // frmSanPhamKH
+    // 
+    this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.ClientSize = new System.Drawing.Size(624, 361);
+    this.Controls.Add(this.btnXemGioHang);
+    this.Controls.Add(this.dgvSanPham);
+    this.Name = "frmSanPhamKH";
+    this.Text = "Sản phẩm";
+    ((System.ComponentModel.ISupportInitialize)(this.dgvSanPham)).EndInit();
+    this.ResumeLayout(false);
         }
 
         #endregion
