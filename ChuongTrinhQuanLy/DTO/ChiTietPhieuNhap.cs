fbbf46f6c86a2b0317c0b5fd3a0c3a25d58ff42e
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 03:36:02 +0700

    Update ChiTietPhieuNhap.cs

--- ChuongTrinhQuanLy/DTO/ChiTietPhieuNhap.cs
+++ ChuongTrinhQuanLy/DTO/ChiTietPhieuNhap.cs
@@ -2,10 +2,9 @@ namespace DTO
 {
     public class ChiTietPhieuNhap
     {
-        public int MaPhieuNhap { get; set; }
-        public int MaSP { get; set; }
-        public string TenSP { get; set; }
-        public int SoLuong { get; set; }
-        public decimal GiaNhap { get; set; }
+    public int MaPhieuNhap { get; set; }
+    public int MaSP { get; set; }
+    public int SoLuong { get; set; }
+    public decimal GiaNhap { get; set; }
     }
 }
