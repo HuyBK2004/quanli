14b8fc73efba18ac6e91436548d46414ed824293
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 03:49:12 +0700

    Update GioHangDAO.cs

--- ChuongTrinhQuanLy/DataAccess/GioHangDAO.cs
+++ ChuongTrinhQuanLy/DataAccess/GioHangDAO.cs
@@ -4,23 +4,47 @@ using System.Data.SqlClient;
 
 public class GioHangDAO
 {
-        public void ThemVaoGioHang(int maNguoiDung, int maSP, int soLuong)
+    public static void ThemVaoGioHang(int maNguoiDung, int maSP, int soLuong)
     {
-        GioHangDAO.ThemVaoGioHang(maNguoiDung, maSP, soLuong);
+        string sql = @"IF EXISTS (SELECT 1 FROM GioHang WHERE MaNguoiDung = @maNguoiDung AND MaSP = @maSP)
+                       UPDATE GioHang SET SoLuong = SoLuong + @soLuong WHERE MaNguoiDung = @maNguoiDung AND MaSP = @maSP
+                       ELSE
+                       INSERT INTO GioHang(MaNguoiDung, MaSP, SoLuong) VALUES(@maNguoiDung, @maSP, @soLuong)";
+        DBHelper.ExecuteNonQuery(sql,
+            new SqlParameter("@maNguoiDung", maNguoiDung),
+            new SqlParameter("@maSP", maSP),
+            new SqlParameter("@soLuong", soLuong));
     }
 
-    public void XoaKhoiGioHang(int maNguoiDung, int maSP)
+    public static void XoaKhoiGioHang(int maNguoiDung, int maSP)
     {
-        GioHangDAO.XoaKhoiGioHang(maNguoiDung, maSP);
+        string sql = "DELETE FROM GioHang WHERE MaNguoiDung = @maNguoiDung AND MaSP = @maSP";
+        DBHelper.ExecuteNonQuery(sql,
+            new SqlParameter("@maNguoiDung", maNguoiDung),
+            new SqlParameter("@maSP", maSP));
     }
 
-    public void XoaToanBoGioHang(int maNguoiDung)
+    public static void XoaToanBoGioHang(int maNguoiDung)
     {
-        GioHangDAO.XoaToanBoGioHang(maNguoiDung);
+        string sql = "DELETE FROM GioHang WHERE MaNguoiDung = @maNguoiDung";
+        DBHelper.ExecuteNonQuery(sql,
+            new SqlParameter("@maNguoiDung", maNguoiDung));
     }
 
-    public List<GioHang> LayGioHang(int maNguoiDung)
+    public static List<GioHang> LayGioHang(int maNguoiDung)
     {
-        return GioHangDAO.LayGioHang(maNguoiDung);
+        List<GioHang> ds = new List<GioHang>();
+        string sql = "SELECT * FROM GioHang WHERE MaNguoiDung = @maNguoiDung";
+        DataTable dt = DBHelper.ExecuteQuery(sql, new SqlParameter("@maNguoiDung", maNguoiDung));
+        foreach (DataRow row in dt.Rows)
+        {
+            ds.Add(new GioHang
+            {
+                MaNguoiDung = (int)row["MaNguoiDung"],
+                MaSP = (int)row["MaSP"],
+                SoLuong = (int)row["SoLuong"]
+            });
+        }
+        return ds;
     }
 }
