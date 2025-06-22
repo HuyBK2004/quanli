eaada1ed39fc3ae583e9728118e4084eb2a1a06f
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 03:39:19 +0700

    Update HoaDonDAO.cs

--- ChuongTrinhQuanLy/DataAccess/HoaDonDAO.cs
+++ ChuongTrinhQuanLy/DataAccess/HoaDonDAO.cs
@@ -1,93 +1,47 @@
-using System;
 using System.Collections.Generic;
 using System.Data;
-using MySql.Data.MySqlClient;
-using DTO;
+using System.Data.SqlClient;
 
-namespace DataAccess
+public class HoaDonDAO
 {
-    public class HoaDonDAO
+    public static int ThemHoaDon(HoaDon hd)
     {
-        public static List<HoaDon> GetAll()
-        {
-            string sql = "SELECT * FROM HoaDon";
-            var dt = DBHelper.ExecuteQuery(sql);
-            var list = new List<HoaDon>();
-            foreach (DataRow row in dt.Rows)
-                list.Add(Map(row));
-            return list;
-        }
+        string sql = "INSERT INTO HoaDon(NgayLap, MaNguoiDung, TongTien) OUTPUT INSERTED.MaHoaDon VALUES(@NgayLap, @MaNguoiDung, @TongTien)";
+        var maHoaDon = (int)DBHelper.ExecuteScalar(sql,
+            new SqlParameter("@NgayLap", hd.NgayLap),
+            new SqlParameter("@MaNguoiDung", hd.MaNguoiDung),
+            new SqlParameter("@TongTien", hd.TongTien));
+        return maHoaDon;
+    }
 
-        public static HoaDon GetById(int id)
-        {
-            string sql = "SELECT * FROM HoaDon WHERE MaHoaDon=@ID";
-            var dt = DBHelper.ExecuteQuery(sql, new MySqlParameter("@ID", id));
-            if (dt.Rows.Count == 0) return null;
-            return Map(dt.Rows[0]);
-        }
+    public static void ThemChiTietHoaDon(ChiTietHoaDon ct)
+    {
+        string sql = "INSERT INTO ChiTietHoaDon(MaHoaDon, MaSP, SoLuong, DonGia) VALUES(@MaHoaDon, @MaSP, @SoLuong, @DonGia)";
+        DBHelper.ExecuteNonQuery(sql,
+            new SqlParameter("@MaHoaDon", ct.MaHoaDon),
+            new SqlParameter("@MaSP", ct.MaSP),
+            new SqlParameter("@SoLuong", ct.SoLuong),
+            new SqlParameter("@DonGia", ct.DonGia));
+    }
 
-        public static int Insert(HoaDon hd)
+    public static List<HoaDon> LayDanhSachHoaDon(string maSP = null)
+    {
+        var ds = new List<HoaDon>();
+        string sql = "SELECT DISTINCT hd.* FROM HoaDon hd INNER JOIN ChiTietHoaDon ct ON hd.MaHoaDon = ct.MaHoaDon";
+        if (!string.IsNullOrEmpty(maSP))
+            sql += " WHERE ct.MaSP = @maSP";
+        DataTable dt = DBHelper.ExecuteQuery(sql,
+            string.IsNullOrEmpty(maSP) ? null : new SqlParameter("@maSP", maSP));
+        foreach (DataRow row in dt.Rows)
         {
-            string sql = @"INSERT INTO HoaDon (NgayLap, MaNguoiDung, TongTien)
-                           VALUES (@NgayLap, @MaNguoiDung, @TongTien)";
-            DBHelper.ExecuteNonQuery(sql,
-                new MySqlParameter("@NgayLap", hd.NgayLap),
-                new MySqlParameter("@MaNguoiDung", hd.MaNguoiDung),
-                new MySqlParameter("@TongTien", hd.TongTien)
-            );
-            return Convert.ToInt32(DBHelper.ExecuteScalar("SELECT LAST_INSERT_ID()"));
-        }
-
-        public static bool Update(HoaDon hd)
-        {
-            string sql = @"UPDATE HoaDon SET NgayLap=@NgayLap, MaNguoiDung=@MaNguoiDung, TongTien=@TongTien
-                           WHERE MaHoaDon=@ID";
-            int res = DBHelper.ExecuteNonQuery(sql,
-                new MySqlParameter("@NgayLap", hd.NgayLap),
-                new MySqlParameter("@MaNguoiDung", hd.MaNguoiDung),
-                new MySqlParameter("@TongTien", hd.TongTien),
-                new MySqlParameter("@ID", hd.MaHoaDon)
-            );
-            return res > 0;
-        }
-
-        public static bool Delete(int id)
-        {
-            string sql = "DELETE FROM HoaDon WHERE MaHoaDon=@ID";
-            int res = DBHelper.ExecuteNonQuery(sql, new MySqlParameter("@ID", id));
-            return res > 0;
-        }
-
-        public static List<HoaDon> Search(DateTime? from, DateTime? to)
-        {
-            string sql = "SELECT * FROM HoaDon WHERE 1=1";
-            var param = new List<MySqlParameter>();
-            if (from.HasValue)
+            ds.Add(new HoaDon
             {
-                sql += " AND NgayLap >= @from";
-                param.Add(new MySqlParameter("@from", from.Value));
-            }
-            if (to.HasValue)
-            {
-                sql += " AND NgayLap <= @to";
-                param.Add(new MySqlParameter("@to", to.Value));
-            }
-            var dt = DBHelper.ExecuteQuery(sql, param.ToArray());
-            var list = new List<HoaDon>();
-            foreach (DataRow row in dt.Rows)
-                list.Add(Map(row));
-            return list;
+                MaHoaDon = (int)row["MaHoaDon"],
+                NgayLap = (DateTime)row["NgayLap"],
+                MaNguoiDung = (int)row["MaNguoiDung"],
+                TongTien = (decimal)row["TongTien"]
+            });
         }
-
-        private static HoaDon Map(DataRow row)
-        {
-            return new HoaDon
-            {
-                MaHoaDon = Convert.ToInt32(row["MaHoaDon"]),
-                NgayLap = Convert.ToDateTime(row["NgayLap"]),
-                MaNguoiDung = Convert.ToInt32(row["MaNguoiDung"]),
-                TongTien = Convert.ToDecimal(row["TongTien"])
-            };
-        }
+        return ds;
     }
 }
