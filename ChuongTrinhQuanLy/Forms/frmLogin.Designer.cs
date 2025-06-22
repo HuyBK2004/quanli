804aac4d9f97e0954388e1e487458c0903fc73ef
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 04:11:09 +0700

    Update frmLogin.Designer.cs

--- ChuongTrinhQuanLy/Forms/frmLogin.Designer.cs
+++ ChuongTrinhQuanLy/Forms/frmLogin.Designer.cs
@@ -3,90 +3,72 @@ namespace Forms
     partial class frmLogin
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Button btnDangNhap;
-        private System.Windows.Forms.Button btnDangKy;
+      private System.Windows.Forms.DataGridView dgvHoaDon;
+private System.Windows.Forms.TextBox txtMaSP;
+private System.Windows.Forms.Button btnTim;
+private System.Windows.Forms.Button btnTaoHoaDon;
 
-        private void InitializeComponent()
-        {
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnDangNhap = new System.Windows.Forms.Button();
-            this.btnDangKy = new System.Windows.Forms.Button();
-
-            this.SuspendLayout();
-
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.Text = "Đăng nhập hệ thống";
-            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Location = new System.Drawing.Point(90, 30);
-
-            // 
-            // lblUsername
-            // 
-            this.lblUsername.Text = "Tên đăng nhập:";
-            this.lblUsername.Location = new System.Drawing.Point(40, 90);
-
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.Text = "Mật khẩu:";
-            this.lblPassword.Location = new System.Drawing.Point(40, 130);
-
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.Location = new System.Drawing.Point(160, 87);
-            this.txtUsername.Width = 180;
-
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(160, 127);
-            this.txtPassword.Width = 180;
-            this.txtPassword.PasswordChar = '*';
-
-            // 
-            // btnDangNhap
-            // 
-            this.btnDangNhap.Text = "Đăng nhập";
-            this.btnDangNhap.Location = new System.Drawing.Point(90, 180);
-            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
-
-            // 
-            // btnDangKy
-            // 
-            this.btnDangKy.Text = "Đăng ký";
-            this.btnDangKy.Location = new System.Drawing.Point(210, 180);
-            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
-
-            // 
-            // frmLogin
-            // 
-            this.ClientSize = new System.Drawing.Size(400, 270);
-            this.Controls.Add(this.lblTitle);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.btnDangNhap);
-            this.Controls.Add(this.btnDangKy);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Đăng nhập";
-
-            this.ResumeLayout(false);
-            this.PerformLayout();
-        }
+private void InitializeComponent()
+{
+    this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+    this.txtMaSP = new System.Windows.Forms.TextBox();
+    this.btnTim = new System.Windows.Forms.Button();
+    this.btnTaoHoaDon = new System.Windows.Forms.Button();
+    ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+    this.SuspendLayout();
+    // 
+    // dgvHoaDon
+    // 
+    this.dgvHoaDon.AllowUserToAddRows = false;
+    this.dgvHoaDon.AllowUserToDeleteRows = false;
+    this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+    this.dgvHoaDon.Location = new System.Drawing.Point(12, 52);
+    this.dgvHoaDon.Name = "dgvHoaDon";
+    this.dgvHoaDon.ReadOnly = true;
+    this.dgvHoaDon.Size = new System.Drawing.Size(700, 300);
+    this.dgvHoaDon.TabIndex = 0;
+    // 
+    // txtMaSP
+    // 
+    this.txtMaSP.Location = new System.Drawing.Point(12, 15);
+    this.txtMaSP.Name = "txtMaSP";
+    this.txtMaSP.Size = new System.Drawing.Size(180, 22);
+    this.txtMaSP.TabIndex = 1;
+    // 
+    // btnTim
+    // 
+    this.btnTim.Location = new System.Drawing.Point(200, 13);
+    this.btnTim.Name = "btnTim";
+    this.btnTim.Size = new System.Drawing.Size(90, 26);
+    this.btnTim.TabIndex = 2;
+    this.btnTim.Text = "Tìm hóa đơn";
+    this.btnTim.UseVisualStyleBackColor = true;
+    this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+    // 
+    // btnTaoHoaDon
+    // 
+    this.btnTaoHoaDon.Location = new System.Drawing.Point(622, 13);
+    this.btnTaoHoaDon.Name = "btnTaoHoaDon";
+    this.btnTaoHoaDon.Size = new System.Drawing.Size(90, 26);
+    this.btnTaoHoaDon.TabIndex = 3;
+    this.btnTaoHoaDon.Text = "Tạo hóa đơn";
+    this.btnTaoHoaDon.UseVisualStyleBackColor = true;
+    this.btnTaoHoaDon.Click += new System.EventHandler(this.btnTaoHoaDon_Click);
+    // 
+    // frmHoaDon
+    // 
+    this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.ClientSize = new System.Drawing.Size(724, 371);
+    this.Controls.Add(this.btnTaoHoaDon);
+    this.Controls.Add(this.btnTim);
+    this.Controls.Add(this.txtMaSP);
+    this.Controls.Add(this.dgvHoaDon);
+    this.Name = "frmHoaDon";
+    this.Text = "Quản lý hóa đơn";
+    ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+    this.ResumeLayout(false);
+    this.PerformLayout();
+}
     }
 }
