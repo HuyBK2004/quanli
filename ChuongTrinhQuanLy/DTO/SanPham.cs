782b2544bb56db05b4127077a6412d36212e85bc
HuyBK2004 <zedgod2k4@gmail.com>
2025-06-23 03:32:03 +0700

    Update SanPham.cs

--- ChuongTrinhQuanLy/DTO/SanPham.cs
+++ ChuongTrinhQuanLy/DTO/SanPham.cs
@@ -1,12 +1,9 @@
-namespace DTO
+public class SanPham
 {
-    public class SanPham
-    {
-        public int MaSP { get; set; }
-        public string TenSP { get; set; }
-        public string MoTa { get; set; }
-        public decimal GiaBan { get; set; }
-        public string DonViTinh { get; set; }
-        public string TrangThai { get; set; }
-    }
+    public int MaSP { get; set; }
+    public string TenSP { get; set; }
+    public string MoTa { get; set; }
+    public decimal GiaBan { get; set; }
+    public string DonViTinh { get; set; }
+    public string TrangThai { get; set; } // "DangBan" hoặc "NgungBan"
 }
